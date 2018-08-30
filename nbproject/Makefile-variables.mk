5252f7fb9e8034bfd88ff0f117c146c84b97d2da
Mark Hahn <mark@hahnca.com>
2018-08-29 21:17:22 -0700

    refactoring mcu-bi and mcu-uni into mcu-motors

---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,20 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# B1 configuration
+CND_ARTIFACT_DIR_B1=dist/B1/production
+CND_ARTIFACT_NAME_B1=mcu-motors.production.hex
+CND_ARTIFACT_PATH_B1=dist/B1/production/mcu-motors.production.hex
+CND_PACKAGE_DIR_B1=${CND_DISTDIR}/B1/package
+CND_PACKAGE_NAME_B1=mcu-motors.tar
+CND_PACKAGE_PATH_B1=${CND_DISTDIR}/B1/package/mcu-motors.tar
+# B3 configuration
+CND_ARTIFACT_DIR_B3=dist/B3/production
+CND_ARTIFACT_NAME_B3=mcu-motors.production.null
+CND_ARTIFACT_PATH_B3=dist/B3/production/mcu-motors.production.null
+CND_PACKAGE_DIR_B3=${CND_DISTDIR}/B3/package
+CND_PACKAGE_NAME_B3=mcu-motors.tar
+CND_PACKAGE_PATH_B3=${CND_DISTDIR}/B3/package/mcu-motors.tar
